b9aa6470374593761ce2fc9f3609c98c51f57c20
acocinas <alcogar79@gmail.com>
2025-10-07 21:00:17 +0200

    feat: Initial project setup with Spring Boot

---
+++ src/main/java/com/hackaton/itacademy/ItacademyApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.itacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItacademyApplication.class, args);
+	}
+
+}
